df7c67e81894c3126a4773bd2938818dfec9fff2
Ozan Tekin <ardilozantekin@gmail.com>
2022-12-13 22:41:05 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { Provider } from 'react-redux'
-import store from './redux/config/store'
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )

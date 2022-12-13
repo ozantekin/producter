b44ba8403608134abf1134566b774bb21da8bb9c
Ozan Tekin <ardilozantekin@gmail.com>
2022-12-13 22:30:54 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+class App extends React.Component {
+  render() {
+    return <div>Hello World !</div>
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
